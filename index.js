02596d28afdb4c251effc7847fec907542a781de
jahnava <jahnavalmt@gmail.com>
2017-07-26 19:53:35 -0400

    began adding jQuery. Added red to target 1 and bounce to all buttons.

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 var express = require('express');
 var server = express();
 var port = process.env.PORT || 8080;
-server.use(express.static(__dirname + '/public'));
 
-
-server.get('/', function(req, res){
-  res.sendFile('index.html', {root: __dirname + '/public/html'});
+server.use(express.static( __dirname +'/public'));
+server.get('/',function(request,response){
+  response.sendFile('index.html',{root: __dirname + '/public/html'});
 });
 
-server.listen(port, function(){
-  console.log('Now listening on port...', port);
+
+server.listen(port,function(){
+  console.log('now listening on port...',port);
 });
