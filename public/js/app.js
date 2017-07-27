60525d9436a48253d07ec72440269ef8420fd8c0
jahnava <jahnavalmt@gmail.com>
2017-07-27 10:19:59 -0400

    animated some targets and moved them around using jQuery

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,9 @@
-// $(document).ready(function() {
-//    $("#target1").css("color", "red");
-//    $("button").addClass("animated shake");
-//    $("#target3").addClass("animated fadeOut");
-//  });
+$(document).ready(function() {
+   $("#target1").css("color", "red");
+   $("button").addClass("animated shake");
+   $("#target1").css("color", "red");
+   $("target4").html("<em>#target4");
+   $("#target2").appendTo("#right-well");
+   $("#target3").clone().appendTo("#left-well");
+
+ });
