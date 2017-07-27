055ec9313b7d3cc51fad20ab8298e64e4ff4b57b
jahnava <jahnavalmt@gmail.com>
2017-07-27 17:36:52 -0400

    Added color to the wells and buttons by calling on the parent and child of each group.

--- public/js/app.js
+++ public/js/app.js
@@ -2,8 +2,11 @@ $(document).ready(function() {
    $("#target1").css("color", "red");
    $("button").addClass("animated shake");
    $("#target1").css("color", "red");
-   $("target4").html("<em>#target4");
+   $("#target4").html("<em>#target4");
    $("#target2").appendTo("#right-well");
    $("#target3").clone().appendTo("#left-well");
-
+   $("#target1").parent().css("background-color","blue");
+   $("#target4").parent().css("background-color","red");
+   $("#right-well").children().css("background-color","orange");
+   $("#left-well").children().css("background-color","orange");
  });
