6daa7d43f171b19c0d65e2f07cffa7979ff19933
jahnava <jahnavalmt@gmail.com>
2017-07-26 21:31:19 -0400

    added animate plugin

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,5 @@
-$(document).ready(function() {
-   $("#target1").css("color", "red");
-   $("button").addClass("animated bounce");
- });
+// $(document).ready(function() {
+//    $("#target1").css("color", "red");
+//    $("button").addClass("animated shake");
+//    $("#target3").addClass("animated fadeOut");
+//  });
